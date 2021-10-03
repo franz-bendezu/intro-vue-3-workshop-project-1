8234f9df111c5aa3bd48282c51f839af8020bc8b
Franz Bendezu <franzant20@gmail.com>
2021-10-03 15:03:05 -0500

    setup of first class

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-const app = ''
+const options = {
+  data(){
+      return {
+        title: "Mis actividades",
+        subtitle: "Tengo 5 actividades",
+      }
+  },
+};
+const app = Vue.createApp(options);
